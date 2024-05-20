056b4b94f328e1a7314bd24bb9e5284c0b889fb6
vasanth-raja <vasanthrajark@gmail.com>
2024-05-20 17:37:16 +0530

    initializtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,77 @@
 import './App.css';
+import FormInput from './components/FormInput';
+import { useState } from 'react';
 
 function App() {
+  const [values,setValues]=useState({
+    username:"",
+    email:"",
+    birthday:"",
+    password:"",
+    confirmPassword:"",
+  })
+  const inputs=[
+    {
+      id:1,
+      name:"username",
+      type:"text",
+      placeholder:"Username",
+      errorMessage:"Username should be 3-16 characters and shouldn't include any special characters",
+      label:"Username",
+      required:true
+    },
+    {
+      id:2,
+      name:"email",
+      type:"email",
+      placeholder:"Email",
+      errorMessage:"It must be a valid email Id",
+      label:"Email",
+      required:true
+    },
+    {
+      id:3,
+      name:"birthday",
+      type:"date",
+      placeholder:"Birthday",
+      label:"Birthday"
+    },
+    {
+      id:4,
+      name:"password",
+      type:"password",
+      placeholder:"Password",
+      errorMessage:"Password should be 8-20 charaacters and include at least 1 letter, 1 number and 1 special character",
+      label:"Password",
+      required:true
+    },
+    {
+      id:5,
+      name:"confirmPassword",
+      type:"password",
+      placeholder:"ConfirmPassword",
+      errorMessage:"Passwords dont match",
+      label:"ConfirmPassword",
+      required:true
+    },
+  ]
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+  const onChange=(e)=>{
+    setValues({...values,[e.target.name]:e.target.value})
+  }
+  console.log(values)
   return (
     <div className="App">
-    <form>
-      
+     
+    <form onSubmit={handleSubmit}>
+      <h1>Register</h1>
+    {inputs.map((input)=>{
+          return <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+        })}
+      <button>Submit</button>
     </form>
     </div>
   );
